b00982358a36b6c4f0fe4f0e76e9c6af9734c0a0
PiyanshK <70416022+PiyanshK@users.noreply.github.com>
2021-06-03 20:30:26 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver(
+        (entries) => {
+            document
+                .querySelector(".bar")
+                .classList.toggle("bar--bg", entries[0].intersectionRatio < 0.9);
+        },
+        {
+            threshold: 0.9,
+        }
+    );
+
+    observer.observe(document.querySelector(".header"));
+});
+
+$(document).ready(function() {
+    $('.sumbit').click(function(event) {
+        console.log('button clicked');
+
+        var email = $('.email').val();
+        var message = $('.message').val();
+        var statusElm = $('.status');
+        statusElm.empty();
+
+        if(email.length > 5 && email.includes('@') && email.includes('.')){
+            statusElm.append('<div> Valid Email</div>')
+        } else{
+            statusElm.append('<div> Invalid Email</div>')
+            event.preventDefault()
+        }
+
+        if(message.length >= 15) {
+            statusElm.append('<div>Valid Message</div>')
+        } else{
+            statusElm.append('<div>Invalid Message</div>')
+            event.preventDefault()
+        }
+    })
+})
