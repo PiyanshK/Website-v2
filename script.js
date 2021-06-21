4ac9ed7f7bf5c51823b3be8b915f29f3381ae1b5
Piyansh Kumar <70416022+PiyanshK@users.noreply.github.com>
2021-06-20 18:11:31 -0700

    Full Mobile Response new Color Theme

--- script.js
+++ script.js
@@ -37,3 +37,9 @@ $(document).ready(function () {
     }
   });
 });
+
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
